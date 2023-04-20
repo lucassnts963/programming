a239436f45f1e25c2ab4c2cebc5434207db9ff27
Lucas Santos <lucassnts963@gmail.com>
2023-04-20 19:27:58 +0000

    correçao de ortografia

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ function Home() {
     <main>
       <h1>Programming.PA</h1>
       <p>Olá, me chamo Lucas Santos, sou Engenheiro de Produção de formação e desenvolvedor por paixão. Estou criando esse espaço para publicar um pouco da minha experiência e aprendizado na minha jornada na busca de uma vaga no mercado de trabalho dev</p>
-      <p>Estudo programação desde de 2014, e por influência de terceiros e por falta de maturidade para tomar uma decisão mais coerente as minhas reais aptidões, acabei ingressando na facudade de engenharia de produção, área fantastica que amo demais, porém não no mesmos nível que amo os códigos.</p>
-      <p>Após, inúmeros acontecimentos na vida pessoal e profissional descidir no final do ano de 2022 seguir na busca por uma vaga de desenvolvedor, confesso que não tem sido fácil por varios motivos, porém uma coisa e certa tenho bastante conhecimento porém está tudo uma bagunça. Levando isso em consideração, estou organizando meu conhecimento e estou criando espaços na internet para divulgar esses connhecimento. Assim, esse será um espaço onde centralizarei as informações garimpadas pela internet por mim.</p>
+      <p>Estudo programação desde de 2014, e por influência de terceiros e por falta de maturidade para tomar uma decisão mais coerente as minhas reais aptidões, acabei ingressando na faculdade de engenharia de produção, área fantástica que amo demais, porém não no mesmos nível que amo os códigos.</p>
+      <p>Após, inúmeros acontecimentos na vida pessoal e profissional decidir no final do ano de 2022 seguir na busca por uma vaga de desenvolvedor, confesso que não tem sido fácil por vários motivos, porém uma coisa e certa tenho bastante conhecimento porém está tudo uma bagunça. Levando isso em consideração, estou organizando meu conhecimento e estou criando espaços na internet para divulgar esses conhecimento. Assim, esse será um espaço onde centralizarei as informações garimpadas pela internet por mim.</p>
       <p>Convido a você a favoritar esse site, que muitas coisas interessantes vão surgir por aqui. Valeu!</p>
 
       <ul>
@@ -12,7 +12,7 @@ function Home() {
           <a href="https://github.com/lucassnts963">GitHub</a>
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/lucas-santos-07446190/">Linkedind</a>
+          <a href="https://www.linkedin.com/in/lucas-santos-07446190/">LinkedIn</a>
         </li>
       </ul>
     </main>
