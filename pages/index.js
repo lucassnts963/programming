227b3f302552bcf630489353e2dc5674c7d1e405
Lucas Santos <lucassnts963@gmail.com>
2023-04-20 18:46:33 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+function Home() {
+  return (
+    <main>
+      <h1>Programming.PA</h1>
+      <p>Olá, me chamo Lucas Santos, sou Engenheiro de Produção de formação e desenvolvedor por paixão. Estou criando esse espaço para publicar um pouco da minha experiência e aprendizado na minha jornada na busca de uma vaga no mercado de trabalho dev</p>
+      <p>Estudo programação desde de 2014, e por influência de terceiros e por falta de maturidade para tomar uma decisão mais coerente as minhas reais aptidões, acabei ingressando na facudade de engenharia de produção, área fantastica que amo demais, porém não no mesmos nível que amo os códigos.</p>
+      <p>Após, inúmeros acontecimentos na vida pessoal e profissional descidir no final do ano de 2022 seguir na busca por uma vaga de desenvolvedor, confesso que não tem sido fácil por varios motivos, porém uma coisa e certa tenho bastante conhecimento porém está tudo uma bagunça. Levando isso em consideração, estou organizando meu conhecimento e estou criando espaços na internet para divulgar esses connhecimento. Assim, esse será um espaço onde centralizarei as informações garimpadas pela internet por mim.</p>
+      <p>Convido a você a favoritar esse site, que muitas coisas interessantes vão surgir por aqui. Valeu!</p>
+
+      <ul>
+        <li>
+          <a href="https://github.com/lucassnts963">GitHub</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/lucas-santos-07446190/">Linkedind</a>
+        </li>
+      </ul>
+    </main>
+  )
+}
+
+export default Home
